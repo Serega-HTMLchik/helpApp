55e259a7d5947500009f14bcedbd95755a690083
Serega-HTMLchik <70023791+Serega-HTMLchik@users.noreply.github.com>
2022-04-16 11:33:47 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  const [data, setData] = React.useState("");
+
+  React.useEffect(() => {
+    fetch("http://192.168.0.143:9000/api")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.message);
+      });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Help Victim!</Text>
+      <View style={styles.bleutooth}>
+        <Text style={styles.input}>{!data ? "Loading..." : data}</Text>
+      </View>
+      <Button title="Post to server" />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  bleutooth: {
+    alignItems: "center",
+    backgroundColor: "#ff8f33",
+    borderRadius: 15,
+    height: 100,
+    justifyContent: "center",
+    marginVertical: 10,
+    width: 200,
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    fontSize: 20,
+  },
+  text: {
+    fontSize: 30,
+  },
+});
